14e9ca362f2c932d1d97b2191ec802b923b314e1
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2019-03-30 18:41:21 +0100

    Reset 'demo.py' file to initial status
    To run our own experiments, we can call 'run_pair.py' so 'demo.py' has been reset to its original state

--- demo.py
+++ demo.py
@@ -17,11 +17,8 @@ parser.add_argument(
     help='Visualize (i.e. save) output of flow.')
 args = parser.parse_args()
 
-#im1 = np.array(Image.open('examples/car1.jpg'))
-#im2 = np.array(Image.open('examples/car2.jpg'))
-im1 = np.array(Image.open('/homedtic/fperez/Documents/sample_data/kitti2012_seq45/frames/gray/000045_10.png').convert('LA'))
-im2 = np.array(Image.open('/homedtic/fperez/Documents/sample_data/kitti2012_seq45/frames/gray/000045_11.png').convert('LA'))
-print(im1.shape)
+im1 = np.array(Image.open('examples/car1.jpg'))
+im2 = np.array(Image.open('examples/car2.jpg'))
 im1 = im1.astype(float) / 255.
 im2 = im2.astype(float) / 255.
 
@@ -32,9 +29,8 @@ minWidth = 20
 nOuterFPIterations = 7
 nInnerFPIterations = 1
 nSORIterations = 30
-colType = 1  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
+colType = 0  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
 
-print("image shape: {0}".format(im1.shape))
 s = time.time()
 u, v, im2W = pyflow.coarse2fine_flow(
     im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
